efb893b6a0c5e8ecc3580504396ffb0c7252f50a
Maya A. Jastrzębowska <mayajas@gmail.com>
2024-03-14 21:16:12 +0100

    add surfidst package to requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = [line.strip() for line in f if line.strip()]
 
 setup(
-    name='prf-package',
+    name='prf-workflow',
     version='0.1',
     packages=find_packages(),
     install_requires=requirements,
